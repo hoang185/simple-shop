b5bd336ea430fe9ae30ca850bf34f49ffd3f7f1d
hoangvu <vuhuyhoang185@gmail.com>
2021-11-06 16:48:43 +0700

    ..

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="./public/asset/bootstrap/css/bootstrap.css">
+    <script src="public/asset/bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+<!-- header here-->
+<div id="header"></div>
+
+<!-- slider-banner here-->
+<div id="siderBanner"></div>
+
+<!-- category here-->
+<div id="menu"></div>
+
+<!-- new product here-->
+<div id="newProduct"></div>
+
+<!-- best product-->
+<div id="bestProduct"></div>
+
+<!-- story -->
+<div id="story"></div>
+
+<!-- footer-->
+<div id="footer"></div>
+</body>
+</html>
